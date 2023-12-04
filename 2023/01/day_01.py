5ebfc0733298522f72ea4b61b6edbda36b2d53d2
maggick <maggick@users.noreply.github.com>
2023-12-04 12:14:53 +0100

    day01 part2

--- 2023/01/day_01.py
+++ 2023/01/day_01.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import re
+import regex as re
 
-input_file = "input"
-#input_file = "test01.txt"
+input_file = "test01.txt"
 input_file = "test02.txt"
+input_file = "input"
+
+numbers= {
+        "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6",
+        "seven":"7", "eight":"8", "nine":"9"
+      }
+
 
 if __name__ == '__main__':
     with open(input_file) as f:
         sum = 0
         for line in f.readlines():
-            line_numbers = re.findall(r'\d', line)
-            digit = line_numbers[0] + line_numbers[-1]
+            line_numbers = re.findall(r"\d|one|two|three|four|five|six|seven|eight|nine", line, overlapped=True)
+            try:
+                d1=numbers[line_numbers[0]]
+            except KeyError:
+                d1=line_numbers[0]
+            try:
+                d2=numbers[line_numbers[-1]]
+            except KeyError:
+                d2=line_numbers[-1]
+            digit = d1+d2
+            print(digit)
             sum += int(digit)
-        print (sum)
+    print (sum)
 
